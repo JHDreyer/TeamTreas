82ebb33a987037d98ab11347f90badf55bcd82c8
PENGUINOVERLORD-newb <harryprogrammerodendaal@gmail.com>
2020-07-10 13:18:05 +0200

    name change

--- testing.py
+++ testing.py
@@ -1,16 +1,2 @@
 print('This is a test')
 print('Test is successful')
-
-ek kan julle hoor
-
-my microphone wil nie werk nie. 
-
-werk dit in discord?
-
-ek is in discord kyk gou daar
-
-ek kan julle hoor. probeer my mic uitfigure
-
-watter laptops het julle . Fkn Mac wil nie laat studio code die mic access nie
-
-terug discord toe
